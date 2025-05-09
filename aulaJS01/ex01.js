d5a2c0ff318eaa9b8d68c483115631ca6781411e
Lara <laraleticia8257@gmail.com>
2025-05-09 00:29:55 +0000

    codigo 08/05 java1

---
+++ aulaJS01/ex01.js
@@ -0,0 +1,17 @@
+var nome = "Lara";
+console.log(nome);
+
+nome = "18";
+console.log(typeof nome);
+
+let idade = 18;
+console.log(idade);
+
+idade = "18";
+console.log(idade);
+
+const cidade = "Patos de Minas";
+console.log(cidade);
+
+cidade = "Uberlandia";
+console.log(cidade);
